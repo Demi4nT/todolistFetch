f6d72d53869884ba75192efacddd094856abc254
Demi4nT <demhernandezmont21@gmail.com>
2022-06-18 23:18:47 +0000

    tarea terminada

--- src/js/component/todolist.js
+++ src/js/component/todolist.js
@@ -4,19 +4,21 @@ export const TodoList = () => {
     const [task, setTask] = useState ("");
     const [taskList, settaskList] = useState([]);
     return (
-        <div>
+        <div className='caja'>
             <h1>To dos</h1>
             <input onChange={ (e) => {setTask(e.target.value)}} />
-            <button onClick={ () => settaskList([...taskList, task])
-            } >
+            <button onClick={ () => settaskList([...taskList, task])} >
                 Añadir
             </button>
             {taskList.map( (value, key) => {
-                return (<li key={key}>
+                return (<li className='list-group-item list-group-item-light' key={key}>
                     {value}
-                    <button onClick={ () => {settaskList(taskList.filter( (item, i) => {
-                        return (i !== key)
-                    } ))}} >x</button>
+                    <div className='myDiv'>
+                        <button className='btn btn-warning' onClick={ () => {settaskList(taskList.filter( (item, i) => {
+                            return (i !== key)
+                        } ))}} > x
+                       </button>
+                    </div>
                 </li>)
             }    
             )}
